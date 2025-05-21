78f538cae49b648c1e43469b41dafc6654f49a83
Cora Iberkleid <ciberkleid@gmail.com>
2025-05-20 20:23:24 -0400

    Add missing import

--- artifacts/simple-producer/src/test/java/com/example/simpleproducer/fortune/BaseClass.java
+++ artifacts/simple-producer/src/test/java/com/example/simpleproducer/fortune/BaseClass.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
